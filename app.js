9c2a0534d6e212f3eb17d01514bb6ee6bc1a54d9
reine <bruno.reine@insep.fr>
2022-12-16 13:17:51 +0100

    message deux

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
+console.log("essai de render");
 app.get("/", (req, res) => res.type('html').send(html));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
